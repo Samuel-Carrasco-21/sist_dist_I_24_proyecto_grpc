ad7773968343c64c74134e9246d4b9d1f858b2ed
Jose Carrasco <jsce2021@gmail.com>
2024-05-07 23:03:50 -0400

    part-one: finishing the first version of part one

--- P1/parte_1a.py
+++ P1/parte_1a.py
@@ -2,15 +2,15 @@
 
 # Importar las bibliotecas necesarias para enviar el client stub
 # Completar
-import ... as pb
+import part_one_pb2 as pb
 
 # Completar las elipsis (...)
-solicitud_1 = pb.solicitud_ride(
-    taxi_id=...
-    conductor_id='Sist_dist',
-    pasajero_id=[...],  # ['Codigo_UPB', 'Codigo_UPB+10', 'Codigo_UPB+20']
-    costo_estimado =  ...
+solicitud_1 = pb.TaxiSolicitud(
+    taxi_id=1024,
+    conductor_id='123456',
+    pasajero_id=["80902", "80102", "80112" ],  # ['Codigo_UPB', 'Codigo_UPB+10', 'Codigo_UPB+20']
+    costo_estimado=20
 )
 
 # Imprimir el mensaje de solicitud:
-print("Formato de solicitud:\n",solicitud_1)
+print(f"Formato de solicitud:\n{solicitud_1}")
--- P1/parte_1b.py
+++ P1/parte_1b.py
@@ -1,5 +1,55 @@
 # Completar...
-import ... as pb
+import part_one_pb2 as pb
+import json
+import pickle
 
 # El resto del codigo aca
 
+with open('sample_request_data.json', 'r') as json_file:
+    sample_request_data = json.load(json_file)
+
+# print(sample_request_data)
+
+taxi_id = sample_request_data['taxi_id']
+conductor_id = sample_request_data['conductor_id']
+pasajero_id = sample_request_data['pasajero_id']
+costo_estimado = sample_request_data['costo_estimado']
+result = []
+for key in range(0,100):
+    result.append({
+        "taxi_id": taxi_id[f'{key}'],
+        "conductor_id": conductor_id[f'{key}'],
+        "pasajero_id": pasajero_id[f'{key}'].split(','),
+        "costo_estimado": costo_estimado[f'{key}']
+    })
+
+print(result)
+
+def marshallazingInputs(list_gotten):
+    messages_list = []
+    for message in list_gotten:
+        messageResult = pb.TaxiSolicitud(
+            taxi_id= message['taxi_id'],
+            conductor_id= message['conductor_id'],
+            pasajero_id= message['pasajero_id'],
+            costo_estimado= message['costo_estimado']
+        )
+        messages_list.append(messageResult.SerializeToString())
+
+    return messages_list
+
+
+marshall_sample_request_data = marshallazingInputs(result)
+
+def printList(itemList):
+    for item in itemList:
+        print(item)
+
+printList(marshall_sample_request_data)
+
+sample_request_data_bin = 'output_p1b.bin'
+
+# generate bin:
+with open(sample_request_data_bin, 'wb') as file:
+    pickle.dump(marshall_sample_request_data, file)
+print(f'marshall_sample_request_data saved at {sample_request_data_bin}.bin')
